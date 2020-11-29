9ee6c98d0570e329c7c7dbd2070c87bc5bc4ca19
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-11-29 14:57:31 +0100

    configure http security

---
+++ src/main/java/com/gabriel/client/sociallogin/sociallogin/security/WebSecurity.java
@@ -0,0 +1,21 @@
+package com.gabriel.client.sociallogin.sociallogin.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login();
+
+    }
+}
